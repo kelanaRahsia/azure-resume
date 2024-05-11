d23d3008206b922c020ea72f5719ad22d7bc37dd
amirF. <mohdamirfaris30@gmail.com>
2024-05-12 07:35:32 +0800

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -1,13 +1,15 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
-
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+// functionApiUrl is Api url from azure function 
+// authorization level function : https://getresumecounteramir.azurewebsites.net/api/GetResumeCounter?code=LHAdW92mV7dIpRc5n7QuFiRdCM-_bk0wcuyiIsWJjljAAzFuri-Y1Q==
+const functionApiUrl = 'https://getresumecounteramir.azurewebsites.net/api/GetResumeCounter?'
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => 
 {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API")
